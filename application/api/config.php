4856835fdeb07c915057fd540efa9c9b0165bb49
zhaojie <zhaojie1278@126.com>
2018-04-24 00:22:01 +0800

    sql

--- application/api/config.php
+++ application/api/config.php
@@ -7,9 +7,18 @@
  */
 return [
     'default_return_type'=>'json',
+    /*
+    // local
     'wafer_config' => [
         'appid' => 'wx12de72cf50e9bb0c',
         'appsecret' => 'e0072f74012ce315413e23d3cc5c2723',
+        'wxurl' => 'https://api.weixin.qq.com/sns/jscode2session?',
+        'grant_type' => 'authorization_code',
+    ],*/
+    // online
+    'wafer_config' => [
+        'appid' => 'wxad30c53f5e8f8290',
+        'appsecret' => 'de1330accfe1fe89e904474febc370b3',
         'wxurl' => 'https://api.weixin.qq.com/sns/jscode2session?',
         'grant_type' => 'authorization_code',
     ],
